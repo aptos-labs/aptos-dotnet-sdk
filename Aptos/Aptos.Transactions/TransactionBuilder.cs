2ae103adcf6ff929bcf412f843510eb48f1fc452
Oliver <heliuchuan@gmail.com>
2024-10-20 02:20:37 -0400

    fmt

--- Aptos/Aptos.Transactions/TransactionBuilder.cs
+++ Aptos/Aptos.Transactions/TransactionBuilder.cs
@@ -26,8 +26,8 @@ public static class TransactionBuilder
                     );
                 if (publicKey is FederatedKeylessPublicKey federatedKeylessPublicKey)
                     return new AccountAuthenticatorSingleKey(
-                      federatedKeylessPublicKey,
-                      Keyless.GetSimulationSignature()
+                        federatedKeylessPublicKey,
+                        Keyless.GetSimulationSignature()
                     );
 
                 return new AccountAuthenticatorSingleKey(publicKey, invalidSignature);
